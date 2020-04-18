ec9c7c45144150d097657302af7aa3339fece4fd
JiaxueG <62087221+JiaxueG@users.noreply.github.com>
2020-04-19 03:02:58 +0800

    20200419

--- measured_rocket.py
+++ measured_rocket.py
@@ -10,8 +10,16 @@ def makeCurve(a, b, c, x):
     y=-(a*x**2+b*x)+c
     return y
 
+def KnCalculate(prediction, sensor): # prediction and sensor are accuracy
+    Kn=sensor/(prediction+sensor)
+    return Kn
+
+def Kalman(prediction, sensor, Kn): # prediction and sensor are data
+    result=(1-Kn)*prediction+Kn*sensor
+    return result
+
 if __name__ == "__main__":
-    x=list(range(100))
+    x=list(range(-100, 100))
     y=[]
     for i in x: 
         y.append(addNoise(makeCurve(1, -2, 1000, i), 200))
