209c404ae8deb25a87d0b1d7c461c3a9577c46ea
JiaxueG <62087221+JiaxueG@users.noreply.github.com>
2020-04-19 09:40:34 +0800

    20200419

--- measured_rocket.py
+++ measured_rocket.py
@@ -14,14 +14,39 @@ def KnCalculate(prediction, sensor): # prediction and sensor are accuracy
     Kn=sensor/(prediction+sensor)
     return Kn
 
-def Kalman(prediction, sensor, Kn): # prediction and sensor are data
-    result=(1-Kn)*prediction+Kn*sensor
+def mileagePrediction(v, a, t): 
+    result=v*t+0.5*a*t**2
     return result
 
+def Kalman(prediction, sensor, Kn):  # prediction and sensor are data
+    result = (1-Kn)*prediction+Kn*sensor
+    return result
+
+
 if __name__ == "__main__":
-    x=list(range(-100, 100))
-    y=[]
-    for i in x: 
-        y.append(addNoise(makeCurve(1, -2, 1000, i), 200))
-    plt.plot(x, y, 'ro', markersize=1)
+    x_actual=list(range(-100, 100))
+    y_actual=[]
+    y_measured=[]
+    y_predicted=[]
+    y_kalman=[]
+    prediction_var=0.1
+    odo_var=0.9
+    for i in x_actual: 
+        y_actual.append(makeCurve(1, -2, 1000, i))
+        y_measured.append(addNoise(makeCurve(1, -2, 1000, i), 350))
+        # Kalman
+        y_predicted.append(y_measured[len(y_measured)-1]-y_measured[len(y_measured)-2]+y_measured[-1])
+        Kn = KnCalculate(prediction_var, odo_var)
+        y_kalman.append(y_predicted[-1]*(1-Kn)+y_measured[-1]*Kn)
+        # Kn1 = odo_var/(prediction_var + odo_var)
+        # Kn2 = prediction_var/(prediction_var + odo_var)
+        # y_kalman.append(y_predicted[-1]*Kn1+y_measured[-1]*Kn2)
+        prediction_var = (prediction_var * odo_var) / (prediction_var + odo_var)**2
+
+    plt.plot(x_actual, y_actual, label='actual')
+    plt.plot(x_actual, y_measured, 'ro', markersize=1, label='measured')
+    plt.plot(x_actual, y_predicted, label='predicted')
+    plt.plot(x_actual, y_kalman, label='kalman')
+    
+    plt.legend()
     plt.show()
