ceda095358bd82f3f47fe72f5bff02423567ca5f
JiaxueG <62087221+JiaxueG@users.noreply.github.com>
2020-04-20 09:59:20 +0800

    20200420

--- draft.py
+++ draft.py
@@ -1,2 +1,31 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+
+t=np.linspace(1, 100, 100)
+a=0.5
+position=(a*t**2)/2 
+
+position_noise=position+np.random.normal(0, 120, size=(t.shape[0]))
+plt.plot(t, position, label='actual position')
+plt.plot(t, position_noise, label='measured position')
+
+# the 1st prediction uses measured value
+predicts=[position_noise[0]]
+position_predict=predicts[0]
+
+predict_var=0 # GPS
+odo_var = 120**2  # location sensor
+v_std=50 # velocity sensor
+for i in range(1, t.shape[0]): 
+    dv=(position[i]-position[i-1])+np.random.normal(0, 50)
+    position_predict=position_predict+dv
+    predict_var=predict_var+v_std**2 
+    # Kalman
+    position_predict=position_predict*odo_var/(predict_var+odo_var)+position_noise[i]*predict_var/(predict_var+odo_var)
+    predict_var = (predict_var*odo_var)/(predict_var + odo_var)**2
+    predicts.append(position_predict)
+
+plt.plot(t, predicts, label='kalman filtered position')
+
+plt.legend()
+plt.show()
---
+++ draft_matlab.m
@@ -0,0 +1,28 @@
+clear
+N=200;
+w(1)=0;                     %w为过程噪�?
+w=randn(1,N)
+x(1)=25;
+a=1;                        %a为方程中A(k)
+for k=2:N
+    x(k)=a*x(k-1)+w(k-1);
+end
+V=randn(1,N);               %V为观察噪�?
+q1=std(V);
+Rvv=q1.^2;
+q2=std(x);
+Rxx=q2.^2;
+q3=std(w);
+Rww=q3.^2;
+c=0.2;                      %c为方程中H(k)
+Y=c*x+V;                    %Y为观察�??
+p(1)=0;
+s(1)=0;
+for t=2:N
+    p1(t)=a.^2*p(t-1)+Rww;     %p1为方程中p'
+    b(t)=c*p1(t)/(c.^2*p1(t)+Rvv);
+    s(t)=a*s(t-1)+b(t)*(Y(t)-a*c*s(t-1));
+    p(t)=p1(t)-c*b(t)*p1(t);
+end
+t=1:N;
+plot(t,s,'r',t,Y,'g',t,x,'b');
